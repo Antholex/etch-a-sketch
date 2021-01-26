631f02993e32b490cf665cb1fad9e89ffb8a272e
Alex Wozniak <alex@antholex.co.uk>
2021-01-26 19:35:00 +0000

    Refactor and improve UX
    Refactors the sketch board code to improve readability and reduce number
    of duplicate function calls
    
    It also improves the overall UX of the application by adding labels and
    error messages to the form which resizes the board.

--- scripts/sketch_board.js
+++ scripts/sketch_board.js
@@ -1,21 +1,28 @@
 function sketchPad() {
 
   const canvas = document.querySelector(".canvas");
-  // const redraw = document.querySelector(".redraw");
   const redraw = document.querySelector(".submit-button");
+  const newSize = document.querySelector("#new-board-size");
   const stylesheet = document.styleSheets[0];
+  const inputField = document.querySelector(".form-field");
 
-  let numberOfSquares = 256;
-  drawBoard();
-  colourBoard();
+  redraw.addEventListener("click", () => {
+    redrawBoard();
+  });
 
-  function drawBoard() {
+  function drawBoard(lengthOfSide) {
+    clearBoard();
+
+    let numberOfSquares = lengthOfSide * lengthOfSide;
+
     for(i = 0; i < numberOfSquares; i++) {
       let drawSquare = document.createElement("div");
 
       drawSquare.classList.add("square");
       canvas.appendChild(drawSquare);
     }
+    resizeBoardSquares(lengthOfSide);
+    colourBoard();
   }
 
   function clearBoard() {
@@ -32,8 +39,8 @@ function sketchPad() {
     } ));
   }
 
-  function resizeBoard(size) {
-    let square
+  function resizeBoardSquares(size) {
+    let square;
 
     for (let i = 0; i < stylesheet.cssRules.length; i++) {
       if (stylesheet.cssRules[i].selectorText === ".square") {
@@ -41,43 +48,51 @@ function sketchPad() {
       }
     }
 
-    let height = ((639 / size) - 1)  + "px";
-    let width = ((639 / size) - 1) + "px";
+    //Remove 1 from overall canvas size to acccount for margin at top and right-hand side, then remove margin of each
+    //square before dividing by number of squares
+    let height = ((639 - size) / size) + "px";
+    let width = ((639 - size) / size) + "px";
 
     square.style.setProperty("height", height);
     square.style.setProperty("width", width);
   }
 
-  // function setBoardSize() {
-  //   let size = prompt("Enter value");
-  //   if (size >= 1 && size <= 50) {
-  //     clearBoard();
-  //     numberOfSquares = size * size;
-  //     drawBoard();
-  //     resizeBoard(size);
-  //     colourBoard();
-  //   }
-  //   else if (size === null) {
-  //     return;
-  //   }
-  //   else {
-  //     alert("Please enter a value between 1 & 50");
-  //     return setBoardSize();
-  //   }
-  // }
-
   function redrawBoard() {
-    let size = document.querySelector(".size-input").value;
-    clearBoard();
-    numberOfSquares = size * size;
-    drawBoard();
-    resizeBoard(size);
-    colourBoard();
-    document.querySelector(".size-input").value = '';
-  };
+    let size = newSize.value;
 
-  redraw.addEventListener("click", () => {
-    redrawBoard();
-  });
+    if (size >= 1 && size <= 50) {
+      drawBoard(size);
+      newSize.value = '';
+      removeErrorMessage();
+    }
+    else {
+      removeErrorMessage();
+      errorMessage();
+      newSize.value = '';
+    }
+  }
+
+  function errorMessage() {
+    let errorMessage = document.createElement("div");
+
+    errorMessage.textContent = "Please enter a value between 1 and 50";
+    errorMessage.className = "error-message";
+    newSize.classList.toggle("error-field");
+    inputField.appendChild(errorMessage);
+  }
+
+  function removeErrorMessage() {
+    let errorMessage = document.querySelector(".error-message");
+    if (errorMessage) {
+      inputField.removeChild(errorMessage);
+      newSize.classList.toggle("error-field");
+    }
+    else {
+      return;
+    }
+  }
+
+  // Initalise board on page load
+  drawBoard(16);
 
 }
